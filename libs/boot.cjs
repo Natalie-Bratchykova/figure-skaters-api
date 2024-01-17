6fd5c24f2ec629d1429384c46a734f6888902507
Natalie-Bratchykova <nataliya.bratchykova@gmail.com>
2024-01-17 16:42:55 +0200

    added sequelize before sync() function

--- libs/boot.cjs
+++ libs/boot.cjs
@@ -1,7 +1,7 @@
 module.exports = app =>{
     // set sequelize settings should be done
     // before the server starts
-    app.db.sync().then(()=>{
+    app.db.sequelize.sync().then(()=>{
         app.listen(app.get("port"))
     })
     
